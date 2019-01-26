9bc7dcff220a4cf414c36fbcc201daae502fbd4d
zpei <zhengqing.pei@gmail.com>
2019-01-26 16:34:25 -0500

    Responsive expertise-section

--- src/client/components/expertise-section/expertise.jsx
+++ src/client/components/expertise-section/expertise.jsx
@@ -1,12 +1,21 @@
 import React, { Component } from 'react'
 import $ from 'jquery'
+import MediaQuery from 'react-responsive'
 
 export default class Expertise extends Component {
+  constructor() {
+    super()
+    this.state = {
+      shown: false
+    }
+  }
+
   componentDidMount() {
     this.io = new IntersectionObserver(
       ([entry]) => {
         if(entry.intersectionRatio > 0.25) {
           $(this.container).animate({opacity: 1}, 1000)
+          this.setState({shown: true})
           this.io.unobserve(this.container)
         }
       }, {threshold: [0, 0.25, 0.5]}
@@ -18,9 +27,8 @@ export default class Expertise extends Component {
     const { icon, title, description, bulletpoints = [] } = this.props
     const expertiseStyles = {
       display: 'flex',
-      // justifyContent: 'start',
       padding: '30px 30px',
-      opacity: 0
+      opacity: this.state.shown ? 1 : 0
     }
 
     const titleStyles = {
@@ -32,14 +40,38 @@ export default class Expertise extends Component {
     }
 
     return (
-      <div ref={container => this.container = container} style={expertiseStyles}>
-        <div><img src={icon}/></div>
-        <div style={detailsStyles}>
-          <h2 style={titleStyles}>{title}</h2>
-          {/* <p>{description}</p> */}
-          <ul>{bulletpoints.map((bulletpoint, idx) => <li key={`bulletpoint-${idx}`}>{bulletpoint}</li>)}</ul>
-        </div>
-      </div>
+      <React.Fragment>
+        <MediaQuery maxWidth={1000}>
+        {match => (
+          match ? 
+          <div style={{padding: '50px 0'}} ref={container => this.container = container}>
+            <div className="expertise-header" style={{display: 'flex', justifyContent: 'flex-start', paddingLeft: '40px'}}>
+              <div><img src={icon}/></div>
+              <h2 style={{...titleStyles, marginLeft: '25px', display: 'flex', alignItems: 'center'}}>{title}</h2>
+            </div>
+              {/* <p>{description}</p> */}
+            <div style={{...detailsStyles, margin: 0}}>
+              <ul style={{listStyle: 'circle'}}>{bulletpoints.map((bulletpoint, idx) => 
+                <li style={{fontSize: '26px', lineHeight: '34px', marginBottom: '15px'}} key={`bulletpoint-${idx}`}>{bulletpoint}</li>)}
+              </ul>
+            </div>
+          </div>
+          
+          : ''
+        )} 
+      </MediaQuery>
+
+      <MediaQuery minWidth={1000}>
+        {match => match ? <div ref={container => this.container = container} style={expertiseStyles}>
+          <div><img src={icon}/></div>
+          <div style={detailsStyles}>
+            <h2 style={titleStyles}>{title}</h2>
+            {/* <p>{description}</p> */}
+            <ul>{bulletpoints.map((bulletpoint, idx) => <li key={`bulletpoint-${idx}`}>{bulletpoint}</li>)}</ul>
+          </div>
+        </div> : ''}
+      </MediaQuery>
+      </React.Fragment>
     );
   }
 }
