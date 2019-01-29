a7d3a2ae5d9a27e308dc933c4b71d6d5117bd7d3
zpei <zhengqing.pei@gmail.com>
2019-01-29 09:29:41 -0500

    style improvements and added cors on server

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const path = require('path')
+const cors = require('cors')
+
 const app = express()
 const compression = require('compression')
 
 app.use(compression())
+app.use(cors())
 app.use(express.static(path.resolve(__dirname, '../../dist')))
 app.get('/resume', (req, res) => {
   res.download(path.resolve(__dirname, '../../dist/assets/pdf/resume.pdf'))
